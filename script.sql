7c5833709cd48c2b0997f1d51094dc018a9e6265
aeisolution <info@aeisolution.it>
2016-09-15 13:01:23 +0200

    View vtickets

---
+++ script.sql
@@ -0,0 +1,24 @@
+﻿select tickets.ID AS ID,
+	tickets.ClienteId AS ClienteId,
+	tickets.Data AS Data,
+	tickets.Oggetto AS Oggetto,
+	tickets.Descrizione AS Descrizione,
+	tickets.DataClose AS DataClose,
+	tickets.CategoriaId AS CategoriaId,
+	tickets.StatoId AS StatoId,
+	categorie.Nome AS Categoria,
+	stati.Nome AS Stato,
+	CONCAT(clienti.Cognome, ' ', clienti.Nome) AS Cliente,
+	tickets.OperatoreId AS OperatoreId,
+	CONCAT(operatori.Cognome, ' ', operatori.Nome) AS Operatore
+from 
+	(
+		(
+			(
+				(tickets left join categorie on((tickets.CategoriaId = categorie.ID))) 
+				left join stati on((tickets.StatoId = stati.ID))
+			) 
+			left join clienti on((tickets.ClienteId = clienti.ID))
+		)
+		left join operatori on((tickets.OperatoreId = operatori.ID))
+	)
