df1cc013712afbf57cbc341581c56ca66a78da91
aeisolution <info@aeisolution.it>
2016-09-16 11:01:13 +0200

    bugfix percorso tickets aperti

--- html/layout/sidebar.php
+++ html/layout/sidebar.php
@@ -25,7 +25,7 @@
                 <a href="operatori.php"><i class="fa fa-edit fa-fw"></i> Operatori</a>
             </li>
             <li>
-                <a href="tickets.php"><i class="fa fa-edit fa-fw"></i> Ticket Aperti</a>
+                <a href="tickets_open.php"><i class="fa fa-edit fa-fw"></i> Ticket Aperti</a>
             </li>
             <li>
                 <a href="tickets.php"><i class="fa fa-edit fa-fw"></i> Ticket</a>
